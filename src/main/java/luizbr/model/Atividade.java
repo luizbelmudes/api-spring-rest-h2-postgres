5fbb658b37c2f1f843a798965b730ca80312c824
luizbelmudes <luiz_belmudes@hotmail.com>
2024-07-28 20:37:35 -0300

    primeiro commit

---
+++ src/main/java/luizbr/model/Atividade.java
@@ -0,0 +1,31 @@
+package luizbr.model;
+
+public class Atividade {
+    Long id;
+    String descricao;
+    Categoria categoria;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+}
---
+++ src/main/java/luizbr/model/Categoria.java
@@ -0,0 +1,22 @@
+package luizbr.model;
+
+public class Categoria {
+    Long id;
+    String descricaoCategoria;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricaoCategoria() {
+        return descricaoCategoria;
+    }
+
+    public void setDescricaoCategoria(String descricaoCategoria) {
+        this.descricaoCategoria = descricaoCategoria;
+    }
+}
